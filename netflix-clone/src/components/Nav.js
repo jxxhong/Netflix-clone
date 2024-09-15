5abcfac20797f14a0f32914a8a0ca991e29fbf25
Jhong <ghddl23@naver.com>
2024-09-15 23:49:10 +0900

    Feat : Add Nav

---
+++ netflix-clone/src/components/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+export default function Nav() {
+  const [show, setShow] =useState(false);
+
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      if(window.scrollY > 50){
+        setShow(true);
+      }else{
+        setShow(false);
+      }
+    })
+    return()=>{
+      window.removeEventListener('scroll', ()=>{});
+    };
+  });
+
+  return (
+    <nav className={`nav ${show && 'nav_black'}`}>
+      <img 
+        alt='Netflix Logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/440px-Netflix_2015_logo.svg.png'
+        className='nav_logo'
+        onClick={()=>window.location.reload()}
+      />
+      <img
+        alt='User logged'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117'
+        className='nav_avatar'
+      />
+    </nav>
+  )
+}
